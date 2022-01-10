5c70155d9788382214aea207c621d2580f5d256e
AleksMarinova <aleksandra.d.marinova@gmail.com>
2022-01-10 17:23:02 +0100

    building main components's skeleton

---
+++ src/About.js
@@ -0,0 +1,12 @@
+ const About = () => {
+    return (
+        <div className="about">
+            <h2>About me</h2>
+            <p>
+                I am a web developer with a passion for creating beautiful and functional websites. 
+            </p>
+        </div>
+    );
+}
+
+export default About;
--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from './Header';
+import About from './About';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <About />
     </div>
   );
 }
