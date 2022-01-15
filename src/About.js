205a8fd676e4e00816c51667549f5fc9315a5fc7
AleksMarinova <aleksandra.d.marinova@gmail.com>
2022-01-15 12:44:45 +0100

    added tech and skills section

--- src/About.js
+++ src/About.js
@@ -1,4 +1,10 @@
-import { BsEmojiSmileUpsideDown, BsEmojiSmile } from 'react-icons/bs';
+import { BsEmojiSmileUpsideDown, BsEmojiSmile, BsKanban } from 'react-icons/bs';
+import { IoLogoJavascript } from 'react-icons/io';
+import { DiScrum } from 'react-icons/di';
+import { GrNode } from 'react-icons/gr';
+import { SiExpress, SiMongodb, SiPostgresql, SiReact, SiRedux, SiDocker, SiWebpack, SiAmazonaws, SiHeroku, SiNetlify, SiMocha, SiJest } from 'react-icons/si';
+import {BiGitPullRequest} from 'react-icons/bi';
+import { Icon } from '@iconify/react';
 import { useState } from 'react';
 
  const About = () => {
@@ -13,13 +19,88 @@ import { useState } from 'react';
             </p>
             <br />
             <p>
-            What helped me succeed was my degree in Linguistics and its focus on syntax and computational linguistics. Additionally, my love for problem-solving and my determination to become a professional developer helped me thrive.
+            My degree in Linguistics and its focus on syntax and computational practices played a major role in my success. Additionally, my love for problem-solving and my determination to become a professional developer helped me thrive.
             </p>
             <br/>
             <p>
-            Three months and over 500 coding hours later, I am ready to share my favourite projects, build on top of them and learn new technologies. 
+            Three months and over 500 coding hours later, I am looking forward to joining a new team and contributing to my new company's growth. 
             </p>
             <br/>
+            <div className="about-icons-header">Tech and skills </div>
+            <div className="about-icons">
+                <div className="about-icon">    
+                    <IoLogoJavascript className="icon"/>
+                    <div>JavaScript</div>
+                </div>
+                <div className="about-icon">
+                    <SiReact className="icon" />
+                    <div>React</div>
+                </div>   
+                <div className="about-icon">
+                     <SiRedux className="icon" />
+                    <div>Redux</div>
+                </div>
+                <div className="about-icon">
+                    <GrNode className="icon"/>
+                    <div>Node</div>
+                </div>
+                <div className="about-icon">
+                    <SiExpress className="icon" />
+                    <div>Express</div>
+                </div>
+                <div className="about-icon">
+                    <Icon icon="simple-icons:graphql" className="icon"/>
+                    <div>GraphQL</div>
+                </div>
+                <div className="about-icon">
+                    <SiMongodb className="icon"/>
+                    <div>MongoDB</div>
+                </div>
+                <div className="about-icon">
+                    <SiPostgresql className="icon"/>
+                    <div>PostgreSQL</div>
+                    </div>
+                <div className="about-icon">
+                    <BiGitPullRequest className="icon"/>
+                    <div>Git</div>
+                </div>  
+                <div className="about-icon">
+                    <SiDocker className="icon"/>
+                    <div>Docker</div>
+                </div>  
+                <div className="about-icon">
+                    <SiWebpack className="icon"/>
+                    <div>Webpack</div>
+                </div>
+                <div className="about-icon">
+                    <SiAmazonaws className="icon"/>
+                    <div>AWS</div>
+                </div>
+                <div className="about-icon">
+                    <SiHeroku className="icon"/>
+                    <div>Heroku</div>
+                </div>
+                <div className="about-icon">
+                    <SiNetlify className="icon"/>
+                    <div>Netlify</div>
+                </div> 
+                <div className="about-icon">
+                    <SiMocha className="icon"/>
+                    <div>Mocha</div>
+                </div> 
+                <div className="about-icon">
+                    <SiJest className="icon"/>
+                    <div>Jest</div>
+                </div>
+                <div className="about-icon">
+                    <BsKanban className="icon"/>
+                    <div>Kanban</div>
+                </div>  
+                <div className="about-icon">
+                    <DiScrum className="icon"/>
+                    <div>Scrum</div>
+                </div>        
+            </div>
             <p>
             Check out my GitHub and connect with me on LinkedIn.
             </p>
