95f9f2a50d82e0394d2095f8a92783d8d2eb89b6
AleksMarinova <aleksandra.d.marinova@gmail.com>
2022-01-16 11:46:54 +0100

    alternative styling

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import Header from './Header';
-import About from './About';
-import ProjectsContainer from './ProjectsContainer';
-import Navbar from './Navbar';
-import Footer from './Footer';
-import { BrowserRouter, Routes, Route } from 'react-router-dom'; 
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Header />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<About />} />
-          <Route path="/projects" element={<ProjectsContainer />} />
-        </Routes>
-        <Footer />      
-      </BrowserRouter>
-    </div>
+      <header className="App-header">Fullstack JavaScript Developer</header>
+      <div className="hero-container"><div className="hero">Aleksandra Marinova</div></div>
+      <navbar className="navbar">
+        <div>About</div>
+        <div>Skills</div>
+        <div>Projects</div>
+        <div>Contact</div>  
+      </navbar>
+      
+      <div className="content">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+       Mauris consectetur massa nec nibh sagittis bibendum. Nulla facilisi. In ac faucibus sapien.
+      <br/>
+      <br/>Morbi rutrum odio ante. Nam tempus malesuada ipsum a lacinia. 
+      Suspendisse ut vulputate magna, eu egestas felis. Proin eget justo quam. 
+      Proin interdum diam in risus venenatis sollicitudin. 
+      <br /><br/>Nunc tincidunt ante eu nunc congue lobortis. 
+      Suspendisse ac metus et metus finibus facilisis quis eu massa. 
+      Sed bibendum porttitor elit eget interdum. In ligula lorem, vulputate nec volutpat sed, 
+      blandit ut erat. Vivamus pharetra feugiat enim, molestie egestas massa tincidunt at.
+      </div>
+     </div>
   );
 }
 
