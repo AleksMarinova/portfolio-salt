f052141b53601b0b2de93635e3e322c3d1a77306
AleksMarinova <aleksandra.d.marinova@gmail.com>
2022-01-24 14:23:20 +0100

    fixed spelling

--- src/components/HeroInfo.js
+++ src/components/HeroInfo.js
@@ -4,7 +4,7 @@ const HeroInfo = () => {
    return ( 
       <div className="hero-info">
          <div className="hero-info__subheader">REACT AFICIONADO</div>
-         <div className="hero-info__subheader">NODE CONNOSSEUR</div>
+         <div className="hero-info__subheader">NODE CONNOISSEUR</div>
          <div className="hero-info__subheader">TDD DEVOTEE</div>
          <div className="hero-info__subheader">Express, Redux, GraphQL, MongoDB, PostgreSQL, Mocha, Jest, Supertest</div>
       </div>
